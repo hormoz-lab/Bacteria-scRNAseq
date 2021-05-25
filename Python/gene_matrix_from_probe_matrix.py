bff086bac587e5c0d0e7647e51b3d4005fb3e91e
Seong Ho Pahng <spahng@g.harvard.edu>
2021-05-25 12:32:09 -0400

    fix typos and assignments for counts dict

--- Python/gene_matrix_from_probe_matrix.py
+++ Python/gene_matrix_from_probe_matrix.py
@@ -19,6 +19,7 @@ def gene_matrix_from_probe_matrix(input_path, outdir, map_method):
 
     data = parse_molecule_info('molecule_info_10X.h5', CBs, N_genes)
     assert np.all(data['umi_counts'].todense(), counts.todense())
+    del counts
 
     gene_name = [str.split(b'_')[0].lower() for str in probe_name]
     probe_id = [int(str.split(b'_')[1]) for str in probe_name]
@@ -39,7 +40,13 @@ def gene_matrix_from_probe_matrix(input_path, outdir, map_method):
         [data['umi_counts'][:, idx].sum(axis=1) for idx in gene_idx]
     ).squeeze().T
 
-    if map_method is 'max_probe_in_cell':
+    counts = {
+        'by_probe': data['umi_counts'],
+        'by_gene_total': sum_probe,
+    }
+
+    if map_method == 'max_probe_in_cell':
+
         which_probe_max = np.array(
             [data['umi_counts'][:, idx].argmax(axis=1) for idx in gene_idx]
         ).squeeze().T
@@ -54,6 +61,10 @@ def gene_matrix_from_probe_matrix(input_path, outdir, map_method):
              for (idx, max_idx) in zip(gene_idx, which_probe_max.T)]
         ).squeeze().T
 
+        counts['by_gene_max_probe'] = max_probe_umis,
+        counts['which_probe_max'] = which_probe_max,
+        counts['max_probe_reads'] = max_probe_reads
+
     else:
         bulk_gene = [
             np.array(data['umi_counts'][:, idx].sum(axis=0)).flatten()
@@ -66,17 +77,9 @@ def gene_matrix_from_probe_matrix(input_path, outdir, map_method):
         bulk_median_probe_reads = data['read_counts'][:,
                                                       which_probe_bulk_median]
 
-    counts = {'by_probe': data['umi_coounts'],
-              'by_gene_total': sum_probe}
-
-    if map_method is 'max_probe_in_cell':
-        counts['by_gene_max_probe'] = max_probe_umis,
-        counts['which_probe_max'] = which_probe_max,
-        counts['max_probe_reads'] = max_probe_reads
-    else:
         counts['by_median_bulk_probe'] = data['umi_counts'][:,
                                                             which_proobe_bulk_median],
-        counts['which_proobe_bulk_median'] = which_proobe_bulk_median,
+        counts['which_probe_bulk_median'] = which_probe_bulk_median,
         counts['bulk_median_reads'] = bulk_median_probe_reads
 
     return counts, data, CBs, gene_name
